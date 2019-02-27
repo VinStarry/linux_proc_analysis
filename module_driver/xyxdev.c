aa570a64c5d7c322dba98ccb941a9aa2ef555b2c
vinstarry <vinstarry@outlook.com>
2019-02-27 14:16:27 +0800

    revise module_driver coding sytle

--- module_driver/xyxdev.c
+++ module_driver/xyxdev.c
@@ -20,30 +20,25 @@ static char* devName = "xyxdev";
 
 struct file_operations pStruct = { open:dev_open, release:dev_release, read:dev_read, write:dev_write, };
 
-int init_module()
-{
+int init_module() {
 	int ret;
 	ret = register_chrdev(MAJOR_DEVNUM, devName, &pStruct);
-	if (ret < 0)
-	{
+	if (ret < 0) {
 		printk("Register dev failed!\n");
 		return -1;
 	}
-	else
-	{
+	else {
 		printk("Register dev successfully!\n");
 		return 0;
 	}
 }
 
-void cleanup_module()
-{
+void cleanup_module() {
 	unregister_chrdev(MAJOR_DEVNUM, devName);
 	printk("Unregister dev successfully!\n");
 }
 
-static int dev_open(struct inode *inode, struct file *file)
-{
+static int dev_open(struct inode *inode, struct file *file)   {
     if(dev_mutex)
         return -EBUSY;
     dev_mutex = 1;
@@ -53,27 +48,22 @@ static int dev_open(struct inode *inode, struct file *file)
 	return 0;
 }
 
-static int dev_release(struct inode *inode, struct file *file)
-{
+static int dev_release(struct inode *inode, struct file *file) {      
 	printk("Device released!\n");
 	module_put(THIS_MODULE);
     dev_mutex = 0;
 	return 0;
 }
 
-static ssize_t dev_read(struct file *file, char __user *user, size_t t, loff_t *f)
-{
-	if(copy_to_user(user,message,sizeof(message)))
-	{
+static ssize_t dev_read(struct file *file, char __user *user, size_t t, loff_t *f) {
+	if(copy_to_user(user,message,sizeof(message))) {
 		return -EFAULT;
 	}
 	return sizeof(message);
 }
 
-static ssize_t dev_write(struct file *file, const char __user *user, size_t t, loff_t *f)
-{
-	if(copy_from_user(message,user,sizeof(message)))
-	{
+static ssize_t dev_write(struct file *file, const char __user *user, size_t t, loff_t *f) {
+	if(copy_from_user(message,user,sizeof(message))) {
 		return -EFAULT;
 	}
 	return sizeof(message);
